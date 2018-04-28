7f8717cf825377e69ba01d9c9491312f938a69e8
Zackary Griffin <zackarygriffin20@gmail.com>
2018-04-28 15:37:20 +1000

    Attempt to set up MySQL DB

--- PinelandsMusicSchool/PinelandsMusicSchool/settings.py
+++ PinelandsMusicSchool/PinelandsMusicSchool/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'PinelandsMusicSchool.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'test123',
+        'USER': 'root',
+        'PASSWORD': 'secret',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
