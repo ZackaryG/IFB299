0041965e04114f520d2a8ca70440c2a4ef8e9814
Zackary Griffin <zackarygriffin20@gmail.com>
2018-04-28 20:44:44 +1000

    Added Info page

--- PinelandsMusicSchool/PinelandsMusicSchool/settings.py
+++ PinelandsMusicSchool/PinelandsMusicSchool/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'home.apps.HomeConfig',
+    'accountinformation.apps.AccountinformationConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
