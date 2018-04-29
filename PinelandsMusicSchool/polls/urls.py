78c521b9d734f80d4d5df8f75b22e463f843055b
Jordan <jordan.y.sim97@gmail.com>
2018-04-29 16:54:22 +1000

    Completing Django polls tutorial.
    Up to part 5.

--- PinelandsMusicSchool/polls/urls.py
+++ PinelandsMusicSchool/polls/urls.py
@@ -5,11 +5,11 @@ from . import views
 app_name = 'polls'
 urlpatterns = [
     # ex: /polls/
-    path('', views.index, name='index'), # Executes method views.index
+    path('', views.IndexView.as_view(), name='index'), # Executes method views.index
     # ex: /polls/5/
-    path('<int:question_id>/', views.detail, name='detail'), # Angle brackets indicate question_id is input to views.detail
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'), # Angle brackets indicate question_id is input to views.detail
     # ex: /polls/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     # ex: /polls/5/vote/
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
