54950bb52af4d9affab821020d0419dfdcd8864d
Zackary Griffin <zackarygriffin20@gmail.com>
2018-05-20 19:30:58 +1000

    Teacher application app added
    HTML provided by Nathan

--- PinelandsMusicSchool/PinelandsMusicSchool/settings.py
+++ PinelandsMusicSchool/PinelandsMusicSchool/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'home.apps.HomeConfig',
     'accountinformation.apps.AccountinformationConfig',
+    'teacherapplication.apps.TeacherapplicationConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- PinelandsMusicSchool/PinelandsMusicSchool/urls.py
+++ PinelandsMusicSchool/PinelandsMusicSchool/urls.py
@@ -19,5 +19,6 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accountinformation/', include('accountinformation.urls')),
+    path('teacherapplication/', include('teacherapplication.urls')),
     path('', include('home.urls')),
 ]
