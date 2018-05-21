3469448e4db9c45de36c02a9d8099ba7e60d7653
Jordan <jordan.y.sim97@gmail.com>
2018-05-21 15:55:53 +1000

    Modified active and completed fields to a status

--- PinelandsMusicSchool/contracts/models.py
+++ PinelandsMusicSchool/contracts/models.py
@@ -2,25 +2,62 @@ from django.db import models
 from login.models import Member
 from django.utils import timezone
 
+class AcceptenceError(Exception):
+    def __init__(self, message):
+        self.message = message
+    def __str__(self):
+        return self.message
 
 class Contract(models.Model):
     teacher = models.ForeignKey(Member, related_name='Teacher', on_delete=models.DO_NOTHING, default=None, null=False)
     student = models.ForeignKey(Member, related_name='Student', on_delete=models.DO_NOTHING, default=None, null=False)
 
+    # Possible statuses of contract
+    PENDING = 'P'
+    ACTIVE = 'A'
+    COMPLETED = 'C'
+    STATUS_CHOICES = (
+        (PENDING, 'Pending'),
+        (ACTIVE, 'Active'),
+        (COMPLETED, 'Completed'),
+    )
+
     # Fields indicating contract acceptance
     teacher_accepted = models.BooleanField(default=False)
     student_accepted = models.BooleanField(default=False)
-    contract_active = models.BooleanField(default=False)
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES, default=PENDING)
 
     start_date = models.DateField('Contract Start Date', auto_now_add=True)
 
+    def __str__(self):
+        id = str(self.id)
+        teacher_name = self.teacher.get_last_name()
+        student_name = self.student.get_last_name()
+
+        return("Contract " + id + ' between ' + teacher_name + ' and ' + student_name)
+
     def start_contract(self):
+        '''
+        Starts contract, if both student and teacher have accepted contract.
+        :return: Throws AcceptanceError if neither have accepted
+        '''
         if not self.student_accepted:
-            raise(PermissionError, 'Student has not accepted contract')
+            raise AcceptenceError('Student has not accepted contract')
         elif not self.teacher_accepted:
-            raise(PermissionError, 'Teacher has not accepted contract')
+            raise AcceptenceError('Teacher has not accepted contract')
         else:
-            self.contract_active = True
+            self.status = self.ACTIVE
             self.start_date = timezone.now()
+
+    def end_contract(self):
+        '''
+        Ends contract, if contract is currently active
+        Flags contract as inactive, and participants as not accepted.
+        Flags contract as completed.
+        '''
+        if self.statue == self.ACTIVE:
+            self.teacher_accepted = False
+            self.student_accepted = False
+            self.status = self.COMPLETED
 
 
--- PinelandsMusicSchool/contracts/tests.py
+++ PinelandsMusicSchool/contracts/tests.py
@@ -1,18 +1,65 @@
 from django.test import TestCase
 from login.models import Member
-from contracts.models import Contract
+from contracts.models import Contract, AcceptenceError
 from django.utils import timezone
 
 # Create your tests here.
 
 class ContractTests(TestCase):
 
-    def testContractCreation(self):
+    def createStudentTeacher(self):
         student = Member.objects.create_student('ass@butt.com', 'Heywood', 'Jablowme', '1997-12-10', 'qqqgqq')
         teacher = Member.objects.create_teacher('butt@ass.com', 'Anhass', 'Rhamarr', '1969-04-17', 'qqqgqq')
+        return student, teacher
 
+    def testContractCreation(self):
+        student, teacher = self.createStudentTeacher()
+        '''
+        Test creation of contract between student and teacher
+        '''
         contract = Contract(teacher=teacher, student=student)
         self.assertIsInstance(contract, Contract)
 
+    def testContractVerification(self):
+        student, teacher = self.createStudentTeacher()
+        contract = Contract(teacher=teacher, student=student)
 
+        # Case: Neither accepted
+        # Expected: Acceptance Error Raised
+        with self.assertRaises(AcceptenceError) as context:
+            contract.start_contract()
+            self.assertTrue('This is broken' in str(context.AcceptanceError))
 
+        assert (contract.status == contract.PENDING)
+
+        # Case: Student not accepted
+        # Expected: Acceptance Error Raised
+        contract.teacher_accepted = True
+        contract.student_accepted = False
+
+        with self.assertRaises(AcceptenceError) as context:
+            contract.start_contract()
+            self.assertTrue('This is broken' in str(context.AcceptanceError))
+
+        assert (contract.status == contract.PENDING)
+
+        # Case: Teacher not accepted
+        # Expected: Acceptance Error Raised
+        contract.teacher_accepted = False
+        contract.student_accepted = True
+
+        with self.assertRaises(AcceptenceError) as context:
+            contract.start_contract()
+            self.assertTrue('This is broken' in str(context.AcceptanceError))
+
+        assert (contract.status == contract.PENDING)
+
+        # Case: Both accepted
+        contract.teacher_accepted = True
+        contract.student_accepted = True
+
+        contract.start_contract()
+
+        assert(contract.student_accepted==True)
+        assert(contract.teacher_accepted==True)
+        assert(contract.status == contract.ACTIVE)
