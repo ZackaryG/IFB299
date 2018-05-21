ad7aec8b99b1513fabb0f794e5a2bc826a7f48a7
Jordan <jordan.y.sim97@gmail.com>
2018-05-21 15:14:26 +1000

    Created model for contract.
    Started writing tests for contract creation

--- PinelandsMusicSchool/contracts/models.py
+++ PinelandsMusicSchool/contracts/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from login.models import Member
+from django.utils import timezone
 
-# Create your models here.
+
+class Contract(models.Model):
+    teacher = models.ForeignKey(Member, related_name='Teacher', on_delete=models.DO_NOTHING, default=None, null=False)
+    student = models.ForeignKey(Member, related_name='Student', on_delete=models.DO_NOTHING, default=None, null=False)
+
+    # Fields indicating contract acceptance
+    teacher_accepted = models.BooleanField(default=False)
+    student_accepted = models.BooleanField(default=False)
+    contract_active = models.BooleanField(default=False)
+
+    start_date = models.DateField('Contract Start Date', auto_now_add=True)
+
+    def start_contract(self):
+        if not self.student_accepted:
+            raise(PermissionError, 'Student has not accepted contract')
+        elif not self.teacher_accepted:
+            raise(PermissionError, 'Teacher has not accepted contract')
+        else:
+            self.contract_active = True
+            self.start_date = timezone.now()
+
+
--- PinelandsMusicSchool/contracts/tests.py
+++ PinelandsMusicSchool/contracts/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from login.models import Member
+from contracts.models import Contract
+from django.utils import timezone
 
 # Create your tests here.
+
+class ContractTests(TestCase):
+
+    def testContractCreation(self):
+        student = Member.objects.create_student('ass@butt.com', 'Heywood', 'Jablowme', '1997-12-10', 'qqqgqq')
+        teacher = Member.objects.create_teacher('butt@ass.com', 'Anhass', 'Rhamarr', '1969-04-17', 'qqqgqq')
+
+        contract = Contract(teacher=teacher, student=student)
+        self.assertIsInstance(contract, Contract)
+
+
+
