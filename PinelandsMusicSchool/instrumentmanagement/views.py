98672dd47116ee3a760f720d427d12b5028ecb66
Zackary Griffin <zackarygriffin20@gmail.com>
2018-05-22 21:50:28 +1000

    Extra restrictions on pages
    Nav bar changes on user, certain pages redirect to the login screen if there is no user. Users can access pages they shouldn't be able to still by typing in the url directly.

--- PinelandsMusicSchool/instrumentmanagement/views.py
+++ PinelandsMusicSchool/instrumentmanagement/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required(login_url='../login/')
 def instrumentmanagement(request):
     return render(request, 'instrumentmanagement\instrumentmanagementpage.html')
