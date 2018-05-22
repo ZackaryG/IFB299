9e69267416cb745043ad529acb869a5b87f0bc2e
Zackary Griffin <zackarygriffin20@gmail.com>
2018-05-22 19:54:07 +1000

    Teacher application creates new users

--- PinelandsMusicSchool/teacherapplication/views.py
+++ PinelandsMusicSchool/teacherapplication/views.py
@@ -1,6 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from login.forms import *
 
 # Create your views here.
 def teacherapplication(request):
+    return render(request, 'teacherapplication/teacherapplicationpage.html')
+
+
+def add(request):
+    email = request.POST.get('Email')
+    first_name = request.POST.get('FirstName')
+    last_name = request.POST.get('LastName')
+    dob = request.POST.get('DOB')
+    password = request.POST.get('Password')
+
+    Member.objects.create_teacher(email, first_name, last_name, dob, password)
     return render(request, 'teacherapplication/teacherapplicationpage.html')
