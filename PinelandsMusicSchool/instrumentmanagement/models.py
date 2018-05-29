23329abb079789b7a2cb222bcbde7ae2cc02d243
Jordan <jordan.y.sim97@gmail.com>
2018-05-29 15:03:06 +1000

    Modified existing fields in instrument model. Prices changes to Float Fields. Added student username field. Added setter method to modify student username field. Started writing tests for instrument model

--- PinelandsMusicSchool/instrumentmanagement/models.py
+++ PinelandsMusicSchool/instrumentmanagement/models.py
@@ -1,10 +1,44 @@
 from django.db import models
+from login.models import Member
 
 # Create your models here.
 class Instrument(models.Model):
+    '''
+    Django model representing an instrument in inventory
+    '''
+
+    # Instrument quality choices
+    NEW = 'n'
+    USED = 'u'
+    UNSPECIFIED = 'p'
+    QUALITY_CHOICES = ((NEW, 'New'),
+                       (USED, 'Used'),
+                       (UNSPECIFIED, 'Unspecified')
+                       )
+
     type = models.CharField(max_length=100)
     size = models.CharField(max_length=100)
-    quality = models.CharField(max_length=100)
-    available = models.CharField(max_length=100)
-    rentalPrice = models.CharField(max_length=100)
-    salePrice = models.CharField(max_length=100)
+    quality = models.CharField(max_length=2,
+                               choices = QUALITY_CHOICES,
+                               default = UNSPECIFIED)
+    is_sold = models.BooleanField(default = False)
+    rental_price = models.FloatField(default = 12)
+    sale_price = models.FloatField(default = 100)
+    assigned_student = models.CharField(max_length=100, default='None')
+
+    def is_avaliable(self):
+        '''
+        Checks if instrument is avaliable (has no assigned student/is not sold)
+        :return: True if instrument has no assigned student, and is not sold.
+        '''
+        return self.assigned_student == 'None' and not self.is_sold
+
+    def set_assigned_student(self, student_username):
+        try:
+            assert(Member.objects.filter(email=student_username).exists() == True)
+        except:
+            raise ValueError('Student does not exist')
+
+        self.assigned_student = student_username
+
+
--- PinelandsMusicSchool/instrumentmanagement/tests.py
+++ PinelandsMusicSchool/instrumentmanagement/tests.py
@@ -1,3 +1,44 @@
 from django.test import TestCase
+from instrumentmanagement.models import Instrument
+from login.models import Member
 
 # Create your tests here.
+def createInstrument():
+    return Instrument.objects.create(type='Guitar', size='Small', quality=Instrument.NEW, rental_price=5.50, sale_price=100.00)
+
+def createStudent():
+    email = 'ass@butt.com'
+    first_name = 'Heywood'
+    last_name = 'Jablowme'
+    dob = '1969-08-02'
+    password = 'qqqgqq'
+
+    return Member.objects.create_student(email, first_name, last_name, dob, password)
+
+class InstrumentTests(TestCase):
+    def test_create_instrument(self):
+        '''
+        Test if instrument is successfully created, and is avaliable
+        :return:
+        '''
+        guitar = createInstrument()
+        self.assertIsInstance(guitar, Instrument)
+        assert(guitar.is_avaliable() == True)
+
+    def test_assign_to_existing_student(self):
+        # Case: Student exists
+        student = createStudent() # Create student
+        guitar = createInstrument() # Create instrument
+
+        guitar.set_assigned_student(student.get_username())
+
+        assert(guitar.assigned_student == student.get_username())
+
+    def test_assign_to_nonexisting_student(self):
+        # Case: Student does not exist
+        guitar = createInstrument()
+
+        with self.assertRaises(Exception) as context:
+            guitar.set_assigned_student('holy@moses.com')
+
+        self.assertTrue('Student does not exist' in context.exception)
